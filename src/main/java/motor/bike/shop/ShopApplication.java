381a8c1881bd9c09ec71e75e3a7bf269867289d2
PrieurA <alexandre-prieur_student2019@wilder.school>
2020-02-03 12:40:51 +0100

    update for deploiment

--- src/main/java/motor/bike/shop/ShopApplication.java
+++ src/main/java/motor/bike/shop/ShopApplication.java
@@ -4,12 +4,13 @@ import motor.bike.shop.entity.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableConfigurationProperties({
 		FileStorageProperties.class
 })
-public class ShopApplication {
+public class ShopApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopApplication.class, args);
